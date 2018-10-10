1d229671fa9cfdc50eebf6988fe8bc39a1217336
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-10 00:27:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,10 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element){
+  return [element, ...array];
+}
+
+function addElementToBeginningOfArray(array, element){
   array.unshift(element)
+  return array
 }
